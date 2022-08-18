2ede63a71a3cd366e9f56cb39c142645eb6ad302
So Chigusa <so.chigusa.pp@gmail.com>
2022-08-18 03:41:38 -0700

    test branch test

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className='utilStyles.headingMd'>
-        <p>[My self introduction here]</p>
+        <p>Tried adding some self introduction in a new branch!!</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
